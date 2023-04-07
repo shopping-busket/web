5029f629a15b7f747091c96cde6a7cf7722b2303
ilja <ilja.ragnar@gmail.com>
2023-04-07 19:01:20 +0200

    added title to dialog, added invitation status, added the ability to kick a user

--- src/components/ShareDialog.vue
+++ src/components/ShareDialog.vue
@@ -3,19 +3,29 @@
     v-model="openDialog"
     max-width="500px"
   >
-    <v-card>
+    <v-card title="Manage Whitelist">
       <v-card-text>
         <v-list :lines="'one'">
           <v-list-item
             v-for="(whitelist, i) in whitelistedUsers"
             :key="whitelist.listId"
             :title="whitelist.inviteEmail"
-            subtitle="Pending..."
             append-icon="mdi-pencil-outline"
             :ripple="true"
             :prepend-avatar="getGravatar(whitelist)"
             @click="editUserDialog = true; editUserIndex = i"
-          />
+          >
+            <v-list-item-subtitle>
+              <div v-if="whitelist.user == null">
+                <v-icon icon="mdi-account-clock-outline" size="small" />
+                Invitation Pending...
+              </div>
+              <div v-else>
+                <v-icon icon="mdi-account-check-outline" size="small" />
+                Joined
+              </div>
+            </v-list-item-subtitle>
+          </v-list-item>
         </v-list>
 
         <div class="d-flex align-center mt-4">
@@ -43,44 +53,79 @@
 
   <v-dialog v-model="editUserDialog" width="500px" persistent>
     <v-card
+      v-if="editUserIndex != -1"
       :title="`Edit ${whitelistedUsers[editUserIndex].inviteEmail}'s Permissions`"
       subtitle="Manage this users permissions or kick him of your list here."
     >
-      <v-card-text />
-      <v-list select-strategy="classic">
-        <v-list-item
-          value="edit"
-          title="Move, Rename and Check Entries"
-          subtitle="Deleting entries is not included"
-          @click="whitelistedUsers[editUserIndex].canEditEntries = !whitelistedUsers[editUserIndex].canEditEntries"
-        >
-          <template #prepend>
-            <v-list-item-action start>
-              <v-checkbox-btn
-                v-model="whitelistedUsers[editUserIndex].canEditEntries"
-              />
-            </v-list-item-action>
-          </template>
-        </v-list-item>
+      <v-card-text>
+        <v-list select-strategy="classic">
+          <v-list-item
+            value="edit"
+            title="Move, Rename and Check Entries"
+            subtitle="Deleting entries is not included"
+            @click="whitelistedUsers[editUserIndex].canEditEntries = !whitelistedUsers[editUserIndex].canEditEntries"
+          >
+            <template #prepend>
+              <v-list-item-action start>
+                <v-checkbox-btn
+                  v-model="whitelistedUsers[editUserIndex].canEditEntries"
+                />
+              </v-list-item-action>
+            </template>
+          </v-list-item>
 
-        <v-list-item
-          value="delete"
-          title="Delete Entries"
-          subtitle="Moving, renaming and checking entries is not included"
-          @click="whitelistedUsers[editUserIndex].canDeleteEntries = !whitelistedUsers[editUserIndex].canDeleteEntries"
-        >
-          <template #prepend>
-            <v-list-item-action start>
-              <v-checkbox-btn
-                v-model="whitelistedUsers[editUserIndex].canDeleteEntries"
-              />
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-      </v-list>
+          <v-list-item
+            value="delete"
+            title="Delete Entries"
+            subtitle="Moving, renaming and checking entries is not included"
+            @click="whitelistedUsers[editUserIndex].canDeleteEntries = !whitelistedUsers[editUserIndex].canDeleteEntries"
+          >
+            <template #prepend>
+              <v-list-item-action start>
+                <v-checkbox-btn
+                  v-model="whitelistedUsers[editUserIndex].canDeleteEntries"
+                />
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+        </v-list>
+
+        <div class="d-flex align-content-space-between">
+          <v-btn
+            class="flex-grow-1"
+            color="red"
+            variant="tonal"
+            size="small"
+            @click="kickUserIndex = editUserIndex; kickUserConfirmationDialog = true"
+          >
+            Kick User
+          </v-btn>
+        </div>
+      </v-card-text>
       <v-card-actions>
-        <v-btn color="primary" variant="text" block @click="updateUserPermissions(whitelistedUsers[editUserIndex]); editUserDialog = false">
+        <v-btn
+          color="primary" variant="text" block
+          @click="updateUserPermissions(whitelistedUsers[editUserIndex]); editUserDialog = false"
+        >
           Save
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <v-dialog v-model="kickUserConfirmationDialog" width="500px">
+    <v-card
+      title="Confirmation"
+      text="Are you sure that you want to kick this user off your list? They will be unable to access this list!"
+    >
+      <v-card-actions>
+        <v-spacer />
+        <v-btn color="primary" @click="kickUserConfirmationDialog = false">
+          Cancel
+        </v-btn>
+
+        <v-btn color="primary" @click="kickUser(kickUserIndex); kickUserConfirmationDialog = false">
+          Yes, I am sure
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -90,12 +135,15 @@
 <script lang="ts" setup>
 import {
   VBtn,
+  VIcon,
+  VSpacer,
   VCard,
   VCardActions,
   VCardText,
   VDialog,
   VList,
   VListItem,
+  VListItemSubtitle,
   VListItemAction,
   VCheckboxBtn,
   VTextField,
@@ -125,11 +173,14 @@ const openDialog = computed({
 const email: Ref<string> = ref('');
 const editUserDialog = ref(false);
 const editUserIndex = ref(-1);
+const kickUserConfirmationDialog = ref(false);
+const kickUserIndex = ref(-1);
 
 export interface UserWhitelist extends DB {
   id: number,
   user: string,
   inviteEmail: string,
+  inviteSecret?: string,
   listId: string,
 
   canEditEntries: boolean,
@@ -164,6 +215,14 @@ async function updateUserPermissions(whitelist: UserWhitelist) {
     canEditEntries: whitelist.canEditEntries,
     canDeleteEntries: whitelist.canDeleteEntries,
   } as Partial<UserWhitelist>);
+}
+
+async function kickUser(index: number) {
+  editUserIndex.value = -1;
+  editUserDialog.value = false;
+
+  await feathersClient.service(Service.WHITELISTED_USERS).remove(whitelistedUsers[index].id);
+  whitelistedUsers.splice(index, 1);
 }
 
 function getGravatar(whitelist: UserWhitelist): string {
