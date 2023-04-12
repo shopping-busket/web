c4824490c3ed662ca2d2f7cb572d5dc4fa3032d0
justmedev <ilja.ragnar@gmail.com>
2023-04-12 13:59:05 +0200

    added states to show if link is expired or an error occurred

--- src/views/me/list/JoinList.vue
+++ src/views/me/list/JoinList.vue
@@ -1,14 +1,24 @@
 <template>
   <div class="d-flex justify-center align-center h-100 flex-column">
-    <span class="mb-3" style="opacity: 60%">Joining...</span>
-    <v-progress-circular indeterminate color="primary" size="40" />
+    <span class="mb-3" style="opacity: 60%">
+      <span v-if="invalidSecret">
+        Invalid or expired share link.
+      </span>
+      <span v-else-if="error">
+        An unknown error occurred!
+      </span>
+      <span>
+        Joining...
+      </span>
+    </span>
+    <v-progress-circular v-if="!(invalidSecret || error)" indeterminate color="primary" size="40" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { VProgressCircular } from 'vuetify/components';
-import { onMounted } from 'vue';
-import feathersClient, { Service } from '@/feathers-client';
+import { onMounted, ref } from 'vue';
+import feathersClient, { FeathersError, Service } from '@/feathers-client';
 import { UserWhitelist } from '@/components/ShareDialog.vue';
 import { Route } from '@/router';
 import { useRouter } from 'vue-router';
@@ -20,15 +30,25 @@ const props = defineProps<{
 }>();
 
 const router = useRouter();
+const invalidSecret = ref(false);
+const error = ref(false);
 
 onMounted(async () => {
-  const res = await feathersClient.service(Service.WHITELISTED_USERS).patch(props.whitelistId, {
-    inviteSecret: props.secret,
-  } as Partial<UserWhitelist>) as UserWhitelist;
+  try {
+    await feathersClient.service(Service.WHITELISTED_USERS).patch(props.whitelistId, {
+      inviteSecret: props.secret,
+    } as Partial<UserWhitelist>);
 
-  console.log(res);
-  console.log('Joined list!');
-
-  await router.push({ name: Route.DISPLAY_LIST, params: { id: props.id } });
+    await router.push({
+      name: Route.DISPLAY_LIST,
+      params: { id: props.id }
+    });
+  } catch (e) {
+    if ((e as FeathersError).code === 403) {
+      console.log('inviteSecret invalid!');
+      invalidSecret.value = true;
+    }
+    error.value = true;
+  }
 });
 </script>
